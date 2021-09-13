1097b4aba2607a321ca1c562130d2ef308a75247
ali0discord <89341024+ali0discord@users.noreply.github.com>
2021-09-13 17:43:01 +0430

    IR_ADMIN

---
+++ index.js
@@ -0,0 +1,19 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// message console.log
+client.on('message', message => {
+	console.log(message.content);
+});
+
+// Login to Discord with your client's token
+client.login(token);
